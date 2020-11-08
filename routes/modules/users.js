e03b2dc45fe1611f76b9034b91d60f1c2b888bb8
r05323045 <r05323045@ntu.edu.tw>
2020-11-08 10:22:22 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,10 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {
+  console.log(req)
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 module.exports = router
