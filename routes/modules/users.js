ee0f70bee661947dbc67622d0dde4e7264a24d72
r05323045 <r05323045@ntu.edu.tw>
2020-11-08 12:21:14 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('../../models/user')
 const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
@@ -8,5 +9,28 @@ router.post('/login', (req, res) => {
 })
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 module.exports = router
