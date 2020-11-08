05afe3cda9824e99fd2341f7008e279420d69373
r05323045 <r05323045@ntu.edu.tw>
2020-11-08 10:06:42 +0800

    feat: add user modules

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -9,15 +9,3 @@ db.once('open', () => {
   })
   console.log('done')
 })
-/*
-id: el.id,
-name: el.name,
-name_en: el.name_en,
-category: el.category,
-image: el.image,
-location: el.location,
-phone: el.phone,
-google_map: el.google_map,
-rating: el.rating,
-description: el.description
-*/
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-// 引入 home 模組程式碼
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-// 將網址結構符合 / 字串的 request 導向 home 模組
+const users = require('./modules/users')
 router.use('/', home)
 router.use('/restaurants', restaurants)
-// 準備引入路由模組
-// 匯出路由器
+router.use('/users', users)
 module.exports = router
