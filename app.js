bceeb28ad0404b239bf91218ad08d43ac9b119d5
r05323045 <r05323045@ntu.edu.tw>
2020-11-08 12:31:21 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-// app.js
-// require packages used in the project
 const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-// 引用路由器
 const routes = require('./routes')
+
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -27,12 +25,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+usePassport(app)
+
 app.use(routes)
 
-// setting static files
 app.use(express.static('public'))
 
-// start and listen on the Express server
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const passport = require('passport')
 const User = require('../../models/user')
+
 const router = express.Router()
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {
-  console.log(req)
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 router.get('/register', (req, res) => {
   res.render('register')
 })
